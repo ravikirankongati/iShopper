2c15f94b69c613134f733bb393cce48f7c9f44df
Vamshi Penchala <vamshyvamshy@gmail.com>
2020-08-04 21:33:20 +0530

    Removed Merge Conflicts

--- srv/src/server.js
+++ srv/src/server.js
@@ -1,4 +1,4 @@
-<<<<<<< HEAD
+
 // const express = require('express');
 // const app = express();
 
@@ -10,16 +10,4 @@
 // app.listen(port, function () {
 //   console.log('myapp listening on port ' + port);
 // });
-=======
-const express = require('express');
-const app = express();
 
-app.get('/', function (req, res) {
-  res.send('Hello World!');
-});
-
-const port = process.env.PORT || 3000;
-app.listen(port, function () {
-  console.log('myapp listening on port ' + port);
-});
->>>>>>> 0e51c509508b4ca0682920f4dcb55f3987501b55
